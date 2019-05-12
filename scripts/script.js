d7b2182c836c2e6c2a747d82973f8b0e72b67351
v-pushkar <40196144+v-pushkar@users.noreply.github.com>
2019-05-13 01:11:59 +0200

    add hover decktion for items

--- scripts/script.js
+++ scripts/script.js
@@ -31,12 +31,31 @@ function createGallery(a) {
     // a - array with psrameters for slider:
     var galleryBox = document.querySelector('[data-name="gallery-box"]'); // get main box with items
     let galleryWidth = galleryBox.offsetWidth;
-    console.log("gallert width :"+galleryWidth)
+    console.log("gallert width :" + galleryWidth)
     galleryBox.setAttribute('class', `gallery-box ${classesGalleryBox(a)}`); // add class to main box
     // let gallitems = galleryBox.getElementsByTagName('*'); // select ol child/items 
     let gallitems = galleryBox.children; // select oll child/items 
     galleryBox.appendChild(itemsWrapper(gallitems, g_options, galleryWidth));
     animationCorrect(gallitems, a.structure, galleryWidth);
+
+    // ----------------hover effect
+
+
+    // var galleryBox = document.querySelector('[data-name="gallery-box"]')
+    galleryBox.onmouseover = galleryBox.onmouseout = handler;
+
+    function handler(event) {
+
+        function str(el) {
+            if (!el) return "null"
+            return el.className || el.tagName;
+        }
+
+        event.type == 'mouseover' ? event.target.closest('.item-Wrapp').setAttribute("data-hoverSkale", "true") : event.target.closest('.item-Wrapp').setAttribute("data-hoverSkale", "false");
+
+    }
+    // ---------------- end hover effect
+
 };
 
 function animationCorrect(el, b, w) { // el - arrey items of gallery, b -  type of gallery structure, w - width of gallery box
@@ -53,10 +72,10 @@ function animationCorrect(el, b, w) { // el - arrey items of gallery, b -  type 
         // console.log(`distans of items : ${(elWidth + ofsetElLeft + (docWidth - windWidth)*0.5 )} ; ${(docWidth - (docWidth - windWidth)*0.5  - elWidth / 3)}`)
         if (ofsetElLeft < (xx[i].offsetWidth / 3)) {
             xx[i].classList.add("transl-to-right")
-        // } else if (((elWidth + ofsetElLeft) <= innerWidth) && ((elWidth + ofsetElLeft) > (innerWidth - elWidth / 3))) {
-        } else if ((elWidth + ofsetElLeft + (docWidth - windWidth)*0.5 ) > (docWidth - (docWidth - windWidth)*0.5  - elWidth / 3)) {
+            // } else if (((elWidth + ofsetElLeft) <= innerWidth) && ((elWidth + ofsetElLeft) > (innerWidth - elWidth / 3))) {
+        } else if ((elWidth + ofsetElLeft + (docWidth - windWidth) * 0.5) > (docWidth - (docWidth - windWidth) * 0.5 - elWidth / 3)) {
             xx[i].classList.add(`transl-to-left`);
-            
+
         }
     }
     if (b.structure == "oneRow") {
