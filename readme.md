e30145560e3b9076e12fd5c889bafc7fa87f365d
v-pushkar <40196144+v-pushkar@users.noreply.github.com>
2019-05-17 21:10:52 +0200

    readme_03

--- readme.md
+++ readme.md
@@ -6,9 +6,39 @@
 
 ---
 
+### options for gallery:
+
+g_options = {
+hover_animation: "scale", // - animation for hover-efect
+structure: "table", // "oneRow", "table"
+struct_cols: "14-6-8-6", // from 1 - 12, "default" - numbers of items in one row foe diferent screens (fron big screens to mobile). If "default" - 8, 6, 3, 2.
+trackBox_teg: "div",
+item_wrappteg: "div",
+item_order: "equally", // "mix" "equally"
+item_margin: 3, // "no" - parameter for margins bettwen items
+item_form: "circle", // "rectangle", "circle", "square" - parameter for item form
+item_border: [
+{
+// can be "none" if border is not need;
+color: "#D45801",
+windth: 3, // just number
+radius: 20 // just number
+}
+]
+};
+
+---
+
 ### functions:
 
+- createGallery - main function of gallery
 - TagCreator - get parametrs (tag-name, stiles, attributes) for new DOM-element (tag) end returne new element;
 - animationClass - add actual class for css styles.
 - animationCorrect - add corection of scale effect, for thet block whot stand left or right edge.
-- animationClass - add classes for gallery, for css
+- animationClass - add classes for gallery, for css styles - build struckture of gallery.
+- itemBorder - add border to items
+- itemNavi - not use
+- itemNavCreator - crate abd returne navigation block for gallery items.
+- preViewer - creat new DOMelement, for view big screen
+- itemBoxSize - calculate size of items
+- itemsWrapper - create new item element of gallery
--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ import itemBorder from "/scripts/modules/itemBorder.js";
 import oneRow from "/scripts/modules/oneRow.js";
 import itemsWrapper from "/scripts/modules/itemsWrapper.js";
 import preViewer from "/scripts/modules/itemPreviewer.js";
+import animationCorrect from "/scripts/modules/animationCorrect.js";
 
 // options for view gallery
 const g_options = {
@@ -76,32 +77,4 @@ function createGallery(a) {
   };
 
   // ---------------- preVieuer start
-}
-
-function animationCorrect(el, b, w) {
-  // el - arrey items of gallery, b -  type of gallery structure, w - width of gallery box
-  let xx = document.getElementsByClassName("item-Wrapp");
-  let rowWidth = 0;
-  for (let i = 0; i < xx.length; ++i) {
-    let ofsetElLeft = xx[i].offsetLeft;
-    // console.log(`ofsetleft of item : ${ofsetElLeft}`)
-    // let windWidth = window.innerWidth;
-    let docWidth = window.innerWidth;
-    let windWidth = w;
-    let elWidth = xx[i].offsetWidth;
-    rowWidth = rowWidth + elWidth;
-    // console.log(`distans of items : ${(elWidth + ofsetElLeft + (docWidth - windWidth)*0.5 )} ; ${(docWidth - (docWidth - windWidth)*0.5  - elWidth / 3)}`)
-    if (ofsetElLeft < xx[i].offsetWidth / 3) {
-      xx[i].classList.add("transl-to-right");
-      // } else if (((elWidth + ofsetElLeft) <= innerWidth) && ((elWidth + ofsetElLeft) > (innerWidth - elWidth / 3))) {
-    } else if (
-      elWidth + ofsetElLeft + (docWidth - windWidth) * 0.5 >
-      docWidth - (docWidth - windWidth) * 0.5 - elWidth / 3
-    ) {
-      xx[i].classList.add(`transl-to-left`);
-    }
-  }
-  if (b.structure == "oneRow") {
-    oneRow(rowWidth);
-  }
 }
